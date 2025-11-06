673d46a44a2771c0d69a16b07c4eb47182510957
jotinhakdlk <kudlake.joao@escola.pr.gov.br>
2025-11-06 12:32:09 -0300

    Add files via upload

--- js/filtrapaciente.js
+++ js/filtrapaciente.js
@@ -1,5 +1,12 @@
 const campoFiltro = document.querySelector('#filtrar-tabela');
 
 campoFiltro.addEventListener('input', function(){
-    console.log("Alguém está digitando...")
+
+
+    const pacientes = document.querySelectorAll(".paciente");
+    for(let i = 0; i < pacientes.length; i++){
+        let paciente = pacientes[i];
+        let tdNome = paciente.querySelector(".info-nome");
+        let nome = tdNome.innerText;
+    }
 });
